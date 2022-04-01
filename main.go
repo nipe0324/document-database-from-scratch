c995f42c6fdc83ae733e885fb4e1e71227d0ed39
Shojiro Yanagisawa <nipe0324@gmail.com>
2022-04-02 06:54:09 +0900

    add storage by cockroachdb/pebble

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/cockroachdb/pebble"
 	"github.com/julienschmidt/httprouter"
 )
 
 type server struct {
+	db   *pebble.DB
 	port string
+}
+
+func newServer(database string, port string) (*server, error) {
+	s := server{db: nil, port: port}
+	var err error
+	s.db, err = pebble.Open(database, &pebble.Options{})
+	return &s, err
 }
 
 func (s server) addDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -24,7 +33,12 @@ func (s server) getDocument(w http.ResponseWriter, r *http.Request, _ httprouter
 }
 
 func main() {
-	s := server{"8080"}
+	s, err := newServer("docdb.data", "8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.db.Close()
+
 	router := httprouter.New()
 	router.POST("/docs", s.addDocument)
 	router.GET("/docs", s.searchDocuments)
