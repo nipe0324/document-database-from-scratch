d239a53a2cd47348ee906ef3a8d52d571cfae283
Shojiro Yanagisawa <nipe0324@gmail.com>
2022-04-02 07:23:19 +0900

    move to server logic to server package

--- main.go
+++ main.go
@@ -1,118 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
-	"github.com/cockroachdb/pebble"
-	"github.com/google/uuid"
 	"github.com/julienschmidt/httprouter"
+	"github.com/nipe0324/document-database-from-scratch/server"
 )
 
-type server struct {
-	db   *pebble.DB
-	port string
-}
-
-func newServer(database string, port string) (*server, error) {
-	s := server{db: nil, port: port}
-	var err error
-	s.db, err = pebble.Open(database, &pebble.Options{})
-	return &s, err
-}
-
-func (s server) addDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	dec := json.NewDecoder(r.Body)
-	var document map[string]interface{}
-	err := dec.Decode(&document)
-	if err != nil {
-		jsonResponse(w, nil, err)
-		return
-	}
-
-	id := uuid.New().String()
-
-	bs, err := json.Marshal(document)
-	if err != nil {
-		jsonResponse(w, nil, err)
-		return
-	}
-	err = s.db.Set([]byte(id), bs, pebble.Sync)
-	if err != nil {
-		jsonResponse(w, nil, err)
-		return
-	}
-
-	jsonResponse(w, map[string]interface{}{
-		"id": id,
-	}, nil)
-}
-
-func (s server) searchDocuments(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	panic("Unimplemented")
-}
-
-func (s server) getDocumentById(id []byte) (map[string]interface{}, error) {
-	value, closer, err := s.db.Get(id)
-	if err != nil {
-		return nil, err
-	}
-	defer closer.Close()
-
-	var document map[string]interface{}
-	err = json.Unmarshal(value, &document)
-	return document, err
-}
-
-func (s server) getDocument(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	id := ps.ByName("id")
-
-	document, err := s.getDocumentById([]byte(id))
-	if err != nil {
-		jsonResponse(w, nil, err)
-		return
-	}
-
-	jsonResponse(w, map[string]interface{}{
-		"document": document,
-	}, nil)
-}
-
-func jsonResponse(w http.ResponseWriter, body map[string]interface{}, err error) {
-	data := map[string]interface{}{
-		"body":   body,
-		"status": "ok",
-	}
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-	} else {
-		data["status"] = "error"
-		data["error"] = err.Error()
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	enc := json.NewEncoder(w)
-	err = enc.Encode(data)
-	if err != nil {
-		// TODO: set up panic handler?
-		panic(err)
-	}
-}
-
 func main() {
-	s, err := newServer("docdb.data", "8080")
+	s, err := server.NewServer("docdb.data", "8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer s.db.Close()
+	defer s.Close()
 
 	router := httprouter.New()
-	router.POST("/docs", s.addDocument)
-	router.GET("/docs", s.searchDocuments)
-	router.GET("/docs/:id", s.getDocument)
+	router.POST("/docs", s.AddDocument)
+	router.GET("/docs", s.SearchDocuments)
+	router.GET("/docs/:id", s.GetDocument)
 
-	log.Println("Listening on " + s.port)
-	log.Fatal(http.ListenAndServe(":"+s.port, router))
+	log.Println("Listening on " + s.Port())
+	log.Fatal(http.ListenAndServe(":"+s.Port(), router))
 }
---
+++ server/server.go
@@ -0,0 +1,109 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/google/uuid"
+	"github.com/julienschmidt/httprouter"
+)
+
+type server struct {
+	db   *pebble.DB
+	port string
+}
+
+func NewServer(database string, port string) (*server, error) {
+	s := server{db: nil, port: port}
+	var err error
+	s.db, err = pebble.Open(database, &pebble.Options{})
+	return &s, err
+}
+
+func (s *server) Port() string {
+	return s.port
+}
+
+func (s *server) Close() {
+	s.db.Close()
+}
+
+func (s *server) AddDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	dec := json.NewDecoder(r.Body)
+	var document map[string]interface{}
+	err := dec.Decode(&document)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	id := uuid.New().String()
+
+	bs, err := json.Marshal(document)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+	err = s.db.Set([]byte(id), bs, pebble.Sync)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	jsonResponse(w, map[string]interface{}{
+		"id": id,
+	}, nil)
+}
+
+func (s *server) SearchDocuments(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	panic("Unimplemented")
+}
+
+func (s *server) GetDocument(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
+
+	document, err := s.getDocumentById([]byte(id))
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	jsonResponse(w, map[string]interface{}{
+		"document": document,
+	}, nil)
+}
+
+func (s *server) getDocumentById(id []byte) (map[string]interface{}, error) {
+	value, closer, err := s.db.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	defer closer.Close()
+
+	var document map[string]interface{}
+	err = json.Unmarshal(value, &document)
+	return document, err
+}
+
+func jsonResponse(w http.ResponseWriter, body map[string]interface{}, err error) {
+	data := map[string]interface{}{
+		"body":   body,
+		"status": "ok",
+	}
+	if err == nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		data["status"] = "error"
+		data["error"] = err.Error()
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	enc := json.NewEncoder(w)
+	err = enc.Encode(data)
+	if err != nil {
+		// TODO: set up panic handler?
+		panic(err)
+	}
+}
